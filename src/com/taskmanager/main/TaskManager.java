dc1f6d28bd59847d275e8b74a017176f5a03e009
unknown <joaochecco@gmail.com>
2023-09-28 16:41:03 -0300

    tela simples

--- src/com/taskmanager/main/TaskManager.java
+++ src/com/taskmanager/main/TaskManager.java
@@ -3,12 +3,23 @@ package com.taskmanager.main;
 import com.taskmanager.models.TaskList;
 import com.taskmanager.models.Task;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class TaskManager {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 500;
+
     public static void main(String[] args) {
-        TaskManager app = new TaskManager();
-        app.start();
+        // TaskManager app = new TaskManager();
+        // app.start();
+
+        // JFrame
+        JFrame frame = new JFrame(); // cria um frame
+        frame.setTitle("Task Manager"); // dá um título para o frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // encerra o programa quando fecha a janela
+        frame.setSize(WIDTH, HEIGHT); // dimensões x e y do frame
+        frame.setVisible(true); // deixa o frame visível
     }
 
     public static void menu() {
